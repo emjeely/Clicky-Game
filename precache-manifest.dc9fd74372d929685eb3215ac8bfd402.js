f90f74ae2bb0a6e14c3bb5ebab4f4362550ce21c
Emjeely <emilyumjee@gmail.com>
2019-04-06 12:26:45 -0700

    Updates

---
+++ precache-manifest.dc9fd74372d929685eb3215ac8bfd402.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  },
+  {
+    "revision": "ef4549f3e1f4d3e33afa",
+    "url": "/Clicky-Game/static/js/main.c4591064.chunk.js"
+  },
+  {
+    "revision": "ed67e21e0fbb680b5f8f",
+    "url": "/Clicky-Game/static/js/2.abafc83a.chunk.js"
+  },
+  {
+    "revision": "ef4549f3e1f4d3e33afa",
+    "url": "/Clicky-Game/static/css/main.25ff9ba0.chunk.css"
+  },
+  {
+    "revision": "c655f6e3a579ea8a3c8b11446474c043",
+    "url": "/Clicky-Game/index.html"
+  }
+];
